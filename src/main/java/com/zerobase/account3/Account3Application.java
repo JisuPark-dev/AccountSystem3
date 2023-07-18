4a45dca09c7ddadea3ac95f56eb7d0aa527411df
jisupark-dev <dodlal123tkd@gmail.com>
2023-07-18 11:32:05 +0900

    [commit] first commit

---
+++ src/main/java/com/zerobase/account3/Account3Application.java
@@ -0,0 +1,13 @@
+package com.zerobase.account3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Account3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Account3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/zerobase/account3/Account3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerobase.account3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Account3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
